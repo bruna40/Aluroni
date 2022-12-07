06da74b7d1c7ff274615f7ad130fd5e61a9005f2
Bruna Santiago <brunasanthiago@outlook.com>
2022-12-06 22:25:24 -0300

    terminando a pagina sobre

--- aluroni/src/pages/Sobre/index.tsx
+++ aluroni/src/pages/Sobre/index.tsx
@@ -1,5 +1,38 @@
+import styles from './styles.module.scss';
+import stylesTema from '../../styles/Tema.module.scss';
+import casa from '../../assets/imagens/casa.png';
+import massa1 from '../../assets/imagens/massa1.png';
+import massa2 from '../../assets/imagens/massa2.png';
+
+const imagens = [massa1, massa2];
+
 export function Sobre() {
+
   return (
-    <h1>Sobre</h1>
+    <section>
+      <h3 className={stylesTema.titulo}> Sobre </h3>
+      <div className={styles.sobreNos}>
+        <img  src={casa} alt='casa aluroni'/>
+        <div className={styles.sobreNos__texto}>
+          <p>
+              Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
+          </p>
+          <p>
+              Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto!
+          </p>
+          <p>
+              Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, que harmonizam perfeitamente com o seu parto, seja carne ou massa!
+          </p>
+        </div>
+      </div>
+      <div className={styles.imagens}>
+        {imagens.map((imagem, index) => (
+          <div key={index} className={styles.imagens__imagem}>
+            <img  src={imagem} alt='massa aluroni'/>
+          </div>
+        ))}
+      </div>
+
+    </section>
   );
 }
